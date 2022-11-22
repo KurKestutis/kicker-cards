509b636abd7647d859fee7f6fe8d8122f257154e
Kestutis Kurienius <kurkestutis@gmail.com>
2022-11-22 16:56:09 +0200

    Initial commit

---
+++ src/components/MainLayoutComponents/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.scss";
+
+const Header = (props) => {
+  return (
+    <header className="header">
+      <nav className="header-nav">
+        <ul className="header-nav-list">
+          <li className="header-nav-list-item">Lorem</li>
+          <li className="header-nav-list-item">Lorem</li>
+          <li className="header-nav-list-item">Lorem</li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
